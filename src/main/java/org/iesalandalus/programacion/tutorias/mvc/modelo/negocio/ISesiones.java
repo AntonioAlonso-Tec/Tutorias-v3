33488176585a2c37dd5ea0ac4363ed8fbdd5e391
AntonioAlonso-Tec <56316349+AntonioAlonso-Tec@users.noreply.github.com>
2020-03-04 12:31:44 +0100

    Modificación clase e interfaz Sesiones
    - Añadidido código de lectura y escritura de archivos a la clase Sesiones
    - Modificada la interfaz ISesiones para reflejar este cambio

--- src/main/java/org/iesalandalus/programacion/tutorias/mvc/modelo/negocio/ISesiones.java
+++ src/main/java/org/iesalandalus/programacion/tutorias/mvc/modelo/negocio/ISesiones.java
@@ -8,6 +8,8 @@ import org.iesalandalus.programacion.tutorias.mvc.modelo.dominio.Sesion;
 import org.iesalandalus.programacion.tutorias.mvc.modelo.dominio.Tutoria;
 
 public interface ISesiones {
+	public void comenzar();
+	public void terminar();
 	public List<Sesion> get();
 	public List<Sesion> get(Tutoria tutoria);
 	public int getTamano();
--- src/main/java/org/iesalandalus/programacion/tutorias/mvc/modelo/negocio/ficheros/Sesiones.java
+++ src/main/java/org/iesalandalus/programacion/tutorias/mvc/modelo/negocio/ficheros/Sesiones.java
@@ -1,6 +1,15 @@
 package org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.ficheros;
 
 import javax.naming.OperationNotSupportedException;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -13,10 +22,44 @@ import org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.ISesiones;
 public class Sesiones implements ISesiones {
 
 	private List<Sesion> coleccionSesiones;
-
+	public static final String NOMBRE_FICHERO_SESIONES="datos/sesiones.dat";
 	public Sesiones() {
 
 		coleccionSesiones = new ArrayList<>();
+	}
+	
+	public void comenzar() {
+		File ficheroSesiones=new File(NOMBRE_FICHERO_SESIONES);
+		try(ObjectInputStream entrada=new ObjectInputStream(new FileInputStream(ficheroSesiones))){
+			Sesion sesion=null;
+			do {
+				sesion=(Sesion) entrada.readObject();
+				insertar(sesion);
+			}while(sesion!=null);
+		}catch(ClassNotFoundException e) {
+			System.out.println("ERROR: No se encuentra la clase");
+		}catch(FileNotFoundException e) {
+			System.out.println("ERROR: No se encuentra el archivo");
+		}catch(EOFException e) {
+			System.out.println("Archivo leído satisfactoriamente");
+		}catch(IOException e) {
+			System.out.println("Error de entrada/salida del archivo");
+		}catch(OperationNotSupportedException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public void terminar() {
+		File ficheroSesiones=new File(NOMBRE_FICHERO_SESIONES);
+		try(ObjectOutputStream salida=new ObjectOutputStream(new FileOutputStream(ficheroSesiones))){
+			for(Sesion sesion:coleccionSesiones)
+				salida.writeObject(sesion);
+			System.out.println("Archivo Alumnos escrito satisfactoriamente");
+		} catch (FileNotFoundException e) {
+			System.out.println("No se pudo crear el archivo Alumnos");
+		} catch (IOException e) {
+			System.out.println("rror de entrada/salida del archivo");
+		}
 	}
 
 	public List<Sesion> get() {
